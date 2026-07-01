1480888e25a626c028949a14d41b0391865cc0cc
Hiral <hiralchavda1803@gmail.com>
2026-07-01 10:05:05 +0530

    events

---
+++ 01-06-2026/Event/script.js
@@ -0,0 +1,19 @@
+console.log(document.querySelector('input'));
+
+document.querySelector('input').onfocus = function() 
+{
+    console.log('Input focused');
+}
+
+document.querySelector('input').onchange = function(event)
+{
+    document.querySelector('span').innerHTML = event.target.value;
+}
+
+
+document.querySelector('form').onsubmit = function(event)
+{
+    event.preventDefault();
+    
+    document.querySelector('span').innerHTML = document.querySelector('input').value;
+}
